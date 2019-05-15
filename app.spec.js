48c488abbfd9e5992bc1b7b7463dd6769c4f92cf
Tim Allen <tnelsallen3@gmail.com>
2019-05-14 18:54:32 -0600

    Add database test shell commands

--- app.spec.js
+++ app.spec.js
@@ -2,10 +2,22 @@ var shell = require('shelljs');
 var request = require("supertest");
 var app = require('./app');
 
-describe('Test the root path', () => {
-  test('It should respond to the GET method', () => {
-    return request(app).get("/").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
+describe('api', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+  afterAll(() => {
+    shell.exec('npx sequelize db:seed:undo:all')
+    shell.exec('npx sequelize db:migrate:undo:all')
+  });
+
+  describe('Test the root path', () => {
+    test('should return a 200', () => {
+      return request(app).get("/").then(response => {
+        expect(response.statusCode).toBe(200)
+      })
+    });
   });
 });
